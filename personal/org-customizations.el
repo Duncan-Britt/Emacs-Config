21ad0f7fdbc0397ef7bd06e2212ae62e0740db2e
Duncan Britt <dbru997@gmail.com>
2024-02-04 15:06:20 -0700

    stuff

--- personal/org-customizations.el
+++ personal/org-customizations.el
@@ -1,4 +1,4 @@
-(setq org-format-latex-options (plist-put org-format-latex-options :scale 1.5))
+(setq org-format-latex-options (plist-put org-format-latex-options :scale 1.2))
 
 ;; M-x pixel-scroll-precision-mode
 ;; How to set it in org mode by default?
@@ -11,3 +11,12 @@
 (add-hook 'org-mode-hook #'org-syntax-table-modify)
 
 (setq org-image-actual-width nil)
+
+(setq org-list-allow-alphabetical t)
+
+;; Export to LateX PDF using minted package
+(setq org-latex-listings 'minted
+      org-latex-packages-alist '(("" "minted"))
+      org-latex-pdf-process
+      '("pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+        "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
